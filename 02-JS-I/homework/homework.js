079fe3833039d49395a99ce493567395c215ef8e
BRCOR1 <j.b.rayk@gmail.com>
2022-08-15 23:56:50 -0500

    new

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,14 +28,14 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  'hola'
+  
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  console.log(3 + 2);
 }
 
 function resta(x, y) {
