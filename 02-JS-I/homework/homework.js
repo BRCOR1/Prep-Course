54bea5b1ca2de14e37c54da2773532c7f0f5d038
BRCOR1 <j.b.rayk@gmail.com>
2022-08-15 23:40:44 -0500

    nuevo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'hola';
 const nuevoNum = 0;
 
 // Crea una variable booleana:
-const nuevoBool = "and";
+const nuevoBool = 1;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
